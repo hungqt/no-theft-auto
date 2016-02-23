ec854a7c5b131315fd95e641c718af3a223b9119
Thomas Borresen <Thomas Borresen>
2016-02-23 16:46:31 +0100

    added early stage register

---
+++ NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/AsyncResponse.java
@@ -0,0 +1,8 @@
+package hulatechnologies.notheftautoapp;
+
+/**
+ * Created by thoma on 2/23/2016.
+ */
+public interface AsyncResponse {
+    void processFinish(Integer output);
+}
---
+++ NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/AsyncTaskRegister.java
@@ -0,0 +1,73 @@
+package hulatechnologies.notheftautoapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by thoma on 2/23/2016.
+ */
+public class AsyncTaskRegister extends AsyncTask<JSONObject, Void, Void> {
+
+    JSONArray data = null;
+    URL url = null;
+
+    @Override
+    protected Void doInBackground(JSONObject... params) {
+        HttpURLConnection urlConnection= null;
+        try {
+            url = new URL("http://folk.ntnu.no/thomborr/PT/addDataSecure.php");
+            String message = params[0].toString();
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //Setter at jeg kan sende data til serveren
+            urlConnection.setDoOutput(true);
+            //Setter at jeg kan motta data fra serveren
+            urlConnection.setDoInput(true);
+            //Gjør noe med byte-streamen så ikke hele lagres i minnet samtidig (?)
+            urlConnection.setChunkedStreamingMode(0);
+            //Setter request method til POST
+            urlConnection.setReadTimeout(10000 /*milliseconds*/);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            urlConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+
+            //Send request
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+            out.write(message.getBytes());
+
+            //clean up
+            out.flush();
+            out.close();
+
+            //Receive answer
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Log.d("Response", convertStreamToString(in));
+            in.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+        return null;
+    }
+    public String convertStreamToString(InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+}
