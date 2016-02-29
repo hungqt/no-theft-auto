714eb21c66788bafd0c276552d5e7863bf103499
Thomas Borresen <Thomas Borresen>
2016-02-29 12:52:38 +0100

    Added checkbox for remember me

--- NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/LoginActivity.java
+++ NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/LoginActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,6 +23,7 @@ public class LoginActivity extends AppCompatActivity implements AsyncResponse{
     private Button btnLogin;
     private EditText userText;
     private EditText passText;
+    private CheckBox remMe;
     private AlertDialog alertDialog;
     private AlertDialog alertDialog2;
     private AlertDialog acceptDialog;
@@ -35,6 +37,7 @@ public class LoginActivity extends AppCompatActivity implements AsyncResponse{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        remMe = (CheckBox)findViewById(R.id.remMe);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         userText = (EditText) findViewById(R.id.userText);
         passText = (EditText) findViewById(R.id.passText);
@@ -60,9 +63,10 @@ public class LoginActivity extends AppCompatActivity implements AsyncResponse{
             json.put("username", userText.getText().toString());
             json.put("password", passText.getText().toString());
             data.execute(json);
-            //LaunchAlert();
-            setPrefName(userText.getText().toString());
-            setPrefPass(passText.getText().toString());
+            if(remMe.isChecked()) {
+                setPrefName(userText.getText().toString());
+                setPrefPass(passText.getText().toString());
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -87,25 +91,6 @@ public class LoginActivity extends AppCompatActivity implements AsyncResponse{
             });
             alertDialog2.show();
         }
-    }
-    private void LaunchAlert(){
-        boolean remember = false;
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        // Add the buttons
-        builder.setPositiveButton(R.string.No, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.setNegativeButton(R.string.Yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
-            }
-        });
-        acceptDialog = builder.create();
-        acceptDialog.setTitle("Stay logged in");
-        acceptDialog.setMessage("Do you wish to stay logged in?");
-        acceptDialog.show();
     }
     private void setPrefName(String prefName){
         PreferenceManager.getDefaultSharedPreferences(LoginActivity.this.getApplicationContext()).edit().putString(PrefName, prefName).commit();
