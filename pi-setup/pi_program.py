c34feb889c83d6b1fb5fcc08cc24893ae7af2af1
Kakedragge <simonhst@stud.ntnu.no>
2016-04-11 00:13:43 +0200

    Okay no funka d heilt sekkert...

--- pi-setup/pi_program.py
+++ pi-setup/pi_program.py
@@ -16,6 +16,11 @@ db1 = MySQLdb.connect(host="mysql.stud.ntnu.no",  # your host, usually localhost
                       passwd="nta123",  # your password
                       db="glennchr_nta")  # name of the data base
 
+db2 = MySQLdb.connect(host="mysql.stud.ntnu.no",  # your host, usually localhost
+                      user="glennchr_nta",  # your username
+                      passwd="nta123",  # your password
+                      db="glennchr_nta")  # name of the data base
+
 rpi_id = 1
 
 #cur = db1.cursor()
@@ -163,7 +168,7 @@ def sendNotification(token):
 
 
 def setAlarm(value):
-    cur = db1.cursor()
+    cur = db2.cursor()
     stringValue = str(value)
     try:
         cur.execute("UPDATE glennchr_nta.raspberry_pi SET alarm =(%s)  WHERE rpi_id = (%s)", (stringValue, rpi_id))
@@ -175,7 +180,7 @@ def setAlarm(value):
 # get-metoder
 def getAlarm():
     # lager en cursor som kan kjore sql soringer
-    cur = db1.cursor()
+    cur = db2.cursor()
     cur.execute('''SELECT alarm FROM raspberry_pi WHERE rpi_id = '{0}' '''.format(rpi_id))
     # henter ut verdier fra fetchone
     values = cur.fetchone()
@@ -185,7 +190,7 @@ def getAlarm():
 
 def getToken(username):
     # lager en cursor som kan kjore sql soringer
-    cur = db1.cursor()
+    cur = db2.cursor()
     user = str(username)
     cur.execute('''SELECT token FROM user WHERE username = '{0}' '''.format(username))
     # henter ut verdier fra fetchone
@@ -196,7 +201,7 @@ def getToken(username):
 
 def getCarName():
     # lager en cursor som kan kjore sql soringer
-    cur = db1.cursor()
+    cur = db2.cursor()
     cur.execute('''SELECT car_name FROM raspberry_pi WHERE rpi_id = {0}'''.format(rpi_id))
     # henter ut verdier fra fetchone
     values = cur.fetchone()
@@ -205,7 +210,7 @@ def getCarName():
 
 
 def getUsername():
-    cur = db1.cursor()
+    cur = db2.cursor()
     cur.execute(''' SELECT U.username
                     FROM user U, user_has_rpi UR
                     WHERE {0} = UR.rpi_id AND U.user_id = UR.user_id'''.format(rpi_id))
