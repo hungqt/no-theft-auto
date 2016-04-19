f5230ae4c6426a06c01f83d6bb08166bc4892d37
hungqt <hqthieu@gmail.com>
2016-04-19 16:14:09 +0200

    login logo is done

--- NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/MainActivity.java
+++ NoTheftAutoApp/app/src/main/java/hulatechnologies/notheftautoapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.design.widget.NavigationView;
@@ -20,6 +21,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
